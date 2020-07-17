95df5b6c0d02242a02609c7dfedd3416f4482cac
Noah Rowe <16njr2@queensu.ca>
2020-07-17 10:43:15 -0400

    Changed person creation code

--- Population.py
+++ Population.py
@@ -2,81 +2,61 @@ import numpy as np
 import random
 import Person
 
+AGE_OPTIONS = ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '90+']
+JOB_OPTIONS = ['Essential', 'Commercial']
+HOUSE_OPTIONS = [1,2,3,4,5,6]
+ISOLATION_OPTIONS = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+
+AGE_WEIGHTS = np.ones(len(AGE_OPTIONS))
+JOB_WEIGHTS = np.ones(len(JOB_OPTIONS))
+HOUSE_WEIGHTS = np.ones(len(HOUSE_OPTIONS))
+ISOLATION_WEIGHTS = np.ones(len(ISOLATION_OPTIONS))
+
+
 class Population:
     '''creates a population of people based on the total population
      uses and age distrubution to weight the assignment of ages'''
 
-    def __init__(self, nPop, n0, ageDistrubution, jobDistrubution, householdDistribution, isolationArray):
+    def __init__(self, nPop, n0):
 
         self.people = []  # The list of all people
-        self.suceptible = [] #list of all suceptible individuals
+        self.suceptible = [0]*nPop #list of all suceptible individuals
         self.infected = []  # list of all infected people
         self.recovered = [] # list of recovered people
-        self.household = [] #list of all houses (list that contains all lists of the people in the house)
+        self.household = [0]*nPop #list of all houses (list that contains all lists of the people in the house)
         self.pop = nPop #total population 
+        
+        houseSize = np.random.choice(a=HOUSE_OPTIONS, p=HOUSE_WEIGHTS)
+        houseIndex = 0
+        self.household[houseIndex] = houseSize
 
         for i in range(0, self.pop):
+            # MAKE A PERSON
+            age = np.random.choice(a=AGE_OPTIONS, p=AGE_WEIGHTS)
+            job = np.random.choice(a=jobOptions, p=jobWeights)
+            isolation_tend = np.random.choice(a=ISOLATION_OPTIONS, p=ISOLATION_WEIGHTS)
 
-            #age distrubution
-            Age = (['0-9'] * ageDistrubution[0] + ['10-19'] * ageDistrubution[1]
-                   + ['20-29'] * ageDistrubution[2] + ['30-39'] * ageDistrubution[3]
-                   + ['40-49'] * ageDistrubution[4] + ['50-59'] * ageDistrubution[5]
-                   + ['60-69'] * ageDistrubution[5] + ['70-79'] * ageDistrubution[6]
-                   + ['80-89'] * ageDistrubution[7] + ['90+'] * ageDistrubution[8])
-            #selects an age based on the age distrubution
-            age = random.choice(Age)
-
-
-            #determines the isolation tendancy based on age
-            if age == '0-9':
-                isolation_tendencies = isolationArray[0]
-            elif age == '10-19':
-                isolation_tendencies = isolationArray[1]
-            elif age == '20-29':
-                isolation_tendencies = isolationArray[2]
-            elif age == '30-39':
-                isolation_tendencies = isolationArray[3]
-            elif age == '40-49':
-                isolation_tendencies = isolationArray[4]
-            elif age == '50-59':
-                isolation_tendencies = isolationArray[5]
-            elif age == '60-69':
-                isolation_tendencies = isolationArray[6]
-            elif age == '70-79':
-                isolation_tendencies = isolationArray[7]
-            elif age == '80-89':
-                isolation_tendencies = isolationArray[8]
-            elif age == '90+':
-                isolation_tendencies = isolationArray[9]
-            else:
-                isolation_tendencies = 1
-
-            Job = ['Essential'] * jobDistrubution[0] + ['Commercial'] * jobDistrubution[1]
-
-            #creates a new person
-            NewPerson = Person(index=i, infected=False, recovered=False,infected_day=None, recovered_day=None,
-                        others_infected=None, cure_days=None, recent_infections=None, age=age, job=Job, house_index=0,isolation_tendencies=isolation_tendencies)
-            self.people.append(NewPerson)  # adds new person to list
-
-            # Creation of the households
-            house_number = 0 # the number of houses is 0
-            temporaryPopulationList = len(self.people)  # creates a temporary list
-            for i in range(0, len(self.people)):
-                houseList = [] # creates a list of all people in a house
-                if (temporaryPopulationList > 0):
-                    HousePopProb = (['1'] * householdDistribution[0] + ['2'] * householdDistribution[1]
-                                    + ['3'] * householdDistribution[2] + ['4'] * householdDistribution[3] + ['5'] *
-                                    householdDistribution[4]+ ['6'] * householdDistribution[5])
-                    HousePop = int(random.choice(HousePopProb)) # calculates the number of people in the house
-
-                    for f in range(0, HousePop):
-                        if (temporaryPopulationList > 0):
-                            self.people[i].set_house(house_number) #sets person i into house n
-                            houseList.append(self.people[i]) #adds person to that house list
-                            temporaryPopulationList = temporaryPopulationList - 1  #removes them from the temporary list length
-                            self.household.append(houseList)  # adds the house to the total list of all houses
-                    house_number = house_number + 1
-
+            newPerson = Person(index=i, infected=False, recovered=False, infected_day=None, recovered_day=None, 
+                               others_infected=None, cure_days=None, recent_infections=None, age=age, 
+                               job=job, house_index=0,isolation_tendencies=isolation_tendencies)
+            
+            # ADD A PERSON
+            self.people[i] = newPerson
+            
+            # Increment house info
+            houseSize -= 1
+            if houseSize == 0:
+                houseSize = np.random.choice(HOUSE_OPTIONS)
+                houseIndex += 1
+                self.household[houseIndex] = houseSize
+                
+        # Make sure last household number is right (when it runs out of people to fill
+        if houseSize != self.household[houseIndex]:
+            self.household[houseIndex] = houseSize
+            
+        # Slice household list to the right size
+        self.household = self.household[:houseIndex]
+        
         # infects the initial people
         self.suceptible = self.people
         for i in range(n0):
