4802daeb8df017b2bde44c4de7c3cdf6d5e8b9ca
C Reed <64921881+creed130@users.noreply.github.com>
2020-07-29 14:12:59 -0400

    Update Person.py

--- Person.py
+++ Person.py
@@ -3,11 +3,10 @@ import json
 
 # How long the infection will last
 json_file = open('data.json')
-data = json.load(json_file)
+disease_params = json.load(json_file)
 
-for p in data['recovery']:
-    MIN_DAYS= p['MIN']
-    MAX_DAYS= p['MAX']
+MIN_DAYS= disease_params['recovery'][0]['MIN']
+MAX_DAYS= disease_params['recovery'][0]['MAX']
 # there's also 'AVG', which is the midpoint between severe and mild cases
 # to go with case_severity for when that can be incorporated into the code
 
