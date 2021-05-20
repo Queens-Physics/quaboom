7ec55ac02bb6f8f4c745a5b934452d4ffec50e23
Noah Rowe <16njr2@queensu.ca>
2021-05-20 13:24:54 -0400

    Cleaned Person.py

--- Person.py
+++ Person.py
@@ -1,8 +1,7 @@
-import numpy as np
 import random
+import numpy as np
 
 class Person(object):
-
 
     def __init__(self, index, sim_obj, infected=False, recovered=False, dead=False, hospitalized=False, quarantined=False,
                  quarantined_day=None, infected_day=None, recovered_day=None, death_day=None, others_infected=None,
@@ -33,10 +32,10 @@ class Person(object):
         self.knows_infected = False
         self.will_get_symptoms = False
         self.has_mask = has_mask
-        
+
         # Set the simulaiton object to access the variables
         self.sim_obj = sim_obj
-        
+
     # Return True if infected, False if not
     def is_infected(self):
         return self.infected
@@ -52,40 +51,40 @@ class Person(object):
     #return True if quarantined, False if not
     def is_quarantined(self):
         return self.quarantined
-    
+
     def is_hospitalzied(self):
         return self.hospitalized
 
     #Puts person in quarantine
-    def set_quarantine(self,day):
+    def set_quarantine(self, day):
         self.quarantined_day = day
         self.quarantined = True
 
     #Allows recovered individuals to leave quarantine
     def leave_quarantine(self, day):
-        if self.quarantined_day == None: 
+        if self.quarantined_day is None:
             self.quarantined_day = 0
-        if self.recovered == True or self.dead == True or (day - self.quarantined_day) >= self.sim_obj.quarantine_time:
+        if self.recovered or self.dead or (day - self.quarantined_day) >= self.sim_obj.quarantine_time:
             self.quarantined = False
             return True
         return False
 
     def get_quarantine_day(self):
         return self.quarantined_day
-    
+
     def not_infected_symptoms(self):
         prob_of_symptom = random.random()
-        if (prob_of_symptom <= self.sim_obj.cold_prob):
+        if prob_of_symptom <= self.sim_obj.cold_prob:
             self.show_symptoms = True
         return self.show_symptoms
-            
-    
+
+
     #checks to see if person shows symptoms on the current day
-    def check_symptoms (self,day):
-        if (self.will_get_symptoms == True and (day - self.infected_day) >= self.days_until_symptoms
-            and self.infected == True):
+    def check_symptoms(self, day):
+        if (self.will_get_symptoms and (day - self.infected_day) >= self.days_until_symptoms
+            and self.infected):
             self.show_symptoms = True
-        elif(self.infected == False):
+        elif not self.infected:
             self.show_symptoms = False
         return self.show_symptoms
 
@@ -103,7 +102,7 @@ class Person(object):
 
     def get_case_severity(self):
         return self.case_severity
-    
+
     def get_mask(self):
         return self.has_mask
 
@@ -121,7 +120,7 @@ class Person(object):
                                                           d_params["days_before_symptoms"]["max"])
             
             # If cure days not specified then choose random number inbetween min and max
-            if self.case_severity == 'Mild' or self.case_severity == None: # If severity not specified, choose Mild
+            if self.case_severity == 'Mild' or self.case_severity is None:  # If severity not specified, choose Mild
                 prob_of_symptom = random.random()
                 if (prob_of_symptom > d_params["mild_symptom_prob"]): #probability that the person has mild symtoms
                     # choose number of days after infection when symptoms show
@@ -157,9 +156,9 @@ class Person(object):
                 self.quarantined = False
                 return False
             return True
-        else: # if not self quarantined
+        else:  # if not self quarantined
             # if they aren't in quarantine, check if they know they're infected
-            ##   then put them into quarantine
+            #   then put them into quarantine
             if self.case_severity != "Mild":
                 # if their symtoms are not mild, quarantine if they're infected
                 # assume people in the hospital aren't spreading it either
@@ -204,8 +203,8 @@ class Person(object):
 
                 return True
         return False
-    
-    def check_hospitalized(self, day): # checking that case_severity==death outside of the loop
+
+    def check_hospitalized(self): # checking that case_severity==death outside of the loop
         if self.infected:
             if self.case_severity == 'Hospitalization' or self.case_severity == 'ICU' or self.case_severity == 'Death':
                 self.hospitalized = True
@@ -213,22 +212,22 @@ class Person(object):
             return True
         return False
 
-    def wear_mask(self): #Determines and returns if person is wearing mask
+    def wear_mask(self):  #Determines and returns if person is wearing mask
         mask_options = np.random.uniform()
-        
+
         if self.has_mask:
             if mask_options > self.sim_obj.wear_mask_properly:
-                return False #False = not wearing a mask
+                return False  #False = not wearing a mask
             else:
-                return True #True = wearing a mask
+                return True  #True = wearing a mask
         else:
             return False
-    
-    #Determines what the inward and outward efficiency of the spread will be based on the mask they are wearing
+
+    # Determines what the inward and outward efficiency of the spread will be based on the mask they are wearing
     def mask_type_efficiency(self):
-        if self.has_mask == True and self.mask_type == "Surgical":
+        if self.has_mask and self.mask_type == "Surgical":
             return self.sim_obj.surgical_inward_eff, self.sim_obj.surgical_outward_eff
-        elif self.has_mask == True and self.mask_type == "Non-surgical":
+        elif self.has_mask and self.mask_type == "Non-surgical":
             return self.sim_obj.nonsurgical_inward_eff, self.sim_obj.nonsurgical_outward_eff
         else:
-            return 1, 1 #Not wearing a mask so this will function will not effect their change of getting the virus
+            return 1, 1  #Not wearing a mask so this will function will not effect their change of getting the virus
