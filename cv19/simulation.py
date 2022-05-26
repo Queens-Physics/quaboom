877cab6b0f82e2e74e5c73245d8e21a8f61e6af3
Noah Rowe <16njr2@queensu.ca>
2022-05-26 15:15:46 -0400

    Removed unused variables again

--- cv19/simulation.py
+++ cv19/simulation.py
@@ -323,7 +323,6 @@ class simulation():
             self.new_tests = 0
 
             if day != 0:
-                new_recovered = self.track_recovered[day] - self.track_recovered[day-1]
                 self.track_new_infected[day] = self.track_infected[day]-self.track_infected[day-1]
                 self.track_new_tested[day] = self.track_tested[day] - self.track_tested[day-1]
 
